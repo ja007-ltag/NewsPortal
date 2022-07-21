31ce345d41a2435695078ff75984407682b54517
Aleksey Gladkov <ja007-ltag@yandex.ru>
2022-07-21 19:03:33 +0300

    Implementation of database models. Cleaner up the project. Commit before debugging DB

--- news/models.py
+++ news/models.py
@@ -1,3 +1,75 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Sum
 
-# Create your models here.
+
+class Author(models.Model):
+    # Связь «один к одному» с встроенной моделью пользователей User;
+    author_user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating = models.SmallIntegerField(default=0)  # рейтинг пользователя
+
+    # суммарный рейтинг каждой статьи автора умножается на 3;
+    # суммарный рейтинг всех комментариев автора;
+    # суммарный рейтинг всех комментариев к статьям автора.
+    def update_rating(self):
+        post_rating = self.post_set.agregate(sum_rating=Sum('rating'))
+        post_rating = post_rating.get('sum_rating')
+
+        comment_rating = self.author_user.comment_set.agregate(sum_rating=Sum('rating'))
+        comment_rating = comment_rating('sum_rating')
+
+        self.rating = post_rating * 3 + comment_rating
+        self.save()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+
+
+class Post(models.Model):
+    ARTICLE = 'AR'
+    NEWS = 'NW'
+    CHOICES = [
+        (ARTICLE, 'Статья'),
+        (NEWS, 'Новость'),
+    ]
+
+    post_author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    type = models.CharField(max_length=2, choices=CHOICES, default=NEWS)  # поле с выбором «статья» или «новость»
+    date = models.DateTimeField(auto_now_add=True)
+    post_category = models.ManyToManyField(Category, through='PostCategory')
+    title = models.CharField(max_length=128)
+    text = models.TextField()
+    rating = models.SmallIntegerField(default=0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+    def preview(self):
+        return f'{self.text[:124]} ...'
+
+
+class PostCategory(models.Model):
+    post_through = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category_through = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    comment_post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    rating = models.SmallIntegerField(default=0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
